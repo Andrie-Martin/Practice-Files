675a387069f103cd4b6ba506475501f2c5f8ba75
Liam <liamandriemartin0@gmail.com>
2025-02-15 19:51:11 +0800

    code implementation

--- Python/For_Loops_Exercise-4.py
+++ Python/For_Loops_Exercise-4.py
@@ -1,6 +1,10 @@
 # Using for loop, calculate the following numbers in the given list, then print it in the terminal.
 
 list = [10,20,30,40,50]
+sum = 0
 
 # Code Implementation
 
+for item in list:
+    sum += item
+    print(sum [5])

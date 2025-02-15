48e80b5515252ab9c0f08d96d26c7770defd02a0
Liam <liamandriemartin0@gmail.com>
2025-02-15 19:45:28 +0800

    modified directions

--- Python/For_Loops_Exercise-4.py
+++ Python/For_Loops_Exercise-4.py
@@ -1,4 +1,4 @@
-# Calculate the following numbers in the given list, then print it in the terminal.
+# Using for loop, calculate the following numbers in the given list, then print it in the terminal.
 
 list = [10,20,30,40,50]
 

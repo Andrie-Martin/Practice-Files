0c1045eadd5d0f957ff0a88b789ad6c49b0463c6
Liam <liamandriemartin0@gmail.com>
2025-02-15 20:22:06 +0800

    added code for letter L

--- Python/Nested_Loops_Exercise-5.py
+++ Python/Nested_Loops_Exercise-5.py
@@ -19,4 +19,13 @@ for x in numbers:
 
 
 # Now, do it for the letter L:
+print(" ")
+print(" ")
 
+numbers = [2, 2, 2, 2, 10]
+
+for x in numbers:
+    outcome = ""
+    for y in range(x):
+        outcome += 'x'
+    print(outcome)

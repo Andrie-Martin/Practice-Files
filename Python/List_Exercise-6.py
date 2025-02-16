472e5ae6898cc005bfcb42bfb2e700cdc2e5b84b
Liam <liamandriemartin0@gmail.com>
2025-02-16 16:39:39 +0800

    code implementation

--- Python/List_Exercise-6.py
+++ Python/List_Exercise-6.py
@@ -1,3 +1,11 @@
 # Write a program to remove the duplicates in a list.
 
 list = [1,1,2,2,3,4,4,5,5]
+
+# Code Implementation:
+
+unique_numbers = []
+for list in list:
+    if list not in unique_numbers:
+        unique_numbers.append(list)
+print(unique_numbers)

88fc7f6dcfd528436177dd1d73f0c03aa10ff9d0
Liam <liamandriemartin0@gmail.com>
2025-02-22 19:19:47 +0800

    code implementation

--- Python/Classes_Exercise-7.py
+++ Python/Classes_Exercise-7.py
@@ -1,1 +1,17 @@
 # Create a class named Person that would have a name attribute and a talk method.
+
+# Code implementation:
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def talk (self):
+        print("talk")
+
+
+# Code sample
+
+name = Person("Liam")
+print (name.name)
+name.talk()
